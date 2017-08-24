4d9133f094ef1f0a82767f1144f4cddbd497fcaa
Nicholas Guerra <nick@scriptthread.com>
2017-08-24 00:31:49 -0700

    new

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,23 +8,41 @@ import Task from './Task.jsx';
 
 // App component - represents whole app
 class App extends Component {
-handleSubmit(event) {
-    event.preventDefault();
 
-    // find the text field via react ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+    constructor(props) {
+        super(props);
 
-    Tasks.insert({
-        text,
-        createdAt: new Date(),
-    });
+        this.state = {
+            hideCompleted: false,
+        };
+    }
 
-    // clear form 
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-}
+    handleSubmit(event) {
+        event.preventDefault();
 
+        // find the text field via react ref
+        var text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
+        Tasks.insert({
+            text,
+            createdAt: new Date(),
+        });
+
+        // clear form 
+        ReactDOM.findDOMNode(this.refs.textInput).value = '';
+    }
+
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: !this.state.hideCompleted,
+        });
+    }
     renderTasks() {
-        return this.props.tasks.map((task) => (
+        var filteredTasks = this.props.tasks;
+        if(this.state.hideCompleted) {
+            filteredTasks = filteredTasks.filter(task => !task.checked);
+        }
+
+        return filteredTasks.map((task) => (
             <Task key={task._id} task={task} />
         ));
     }
@@ -33,7 +51,17 @@ handleSubmit(event) {
         return (
             <div className="container">
                 <header>
-                    <h1>Todo List</h1>
+                    <h1>Todo List ({this.props.incompleteCount})</h1>
+
+                    <label className="hide-completed">
+                        <input
+                            type="checkbox"
+                            readOnly
+                            checked={this.state.hideCompleted}
+                            onClick={this.toggleHideCompleted.bind(this)}
+                        />
+                        Hide Completed Tasks
+                    </label>
 
                     <form className="new-task" onSubmit={this.handleSubmit.bind(this)}>
                         <input
@@ -54,10 +82,12 @@ handleSubmit(event) {
 
 App.propTypes = {
     tasks: PropTypes.array.isRequired,
+    incompleteCount: PropTypes.number.isRequired,
 };
 
 export default createContainer(() => {
     return {
         tasks: Tasks.find({}, { sort: {createAt: -1} }).fetch(),
+        incompleteCount: Tasks.find({ checked: {$ne: true} }).count(),
     };
 },App);
--- imports/ui/Task.jsx
+++ imports/ui/Task.jsx
@@ -1,10 +1,39 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Tasks } from '../api/tasks.js';
+
 // Task component - represents a single todo item
 export default class Task extends Component {
+    toggleChecked() {
+        // set the checked property to the opposite of its current value
+        Tasks.update(this.props.task._id, {
+            // set this prop.checked to what ISN'T its current boolean value
+            $set: { checked: !this.props.task.checked },
+        });
+    }
+
+    deleteThisTask() {
+        Tasks.remove(this.props.task._id);
+    }
+
     render() {
+                // is checked true? if yes = 'checked' otherwise false
+        var taskClassName =  this.props.task.checked ? 'checked' : '';
+
         return (
-            <li>{this.props.task.text}<span className="author">{this.props.task.author}</span></li>
+            <li className={taskClassName}>
+                <button className="delete" onClick={this.deleteThisTask.bind(this)}>&times;
+                </button>
+
+                <input
+                    type="checkbox"
+                    readOnly
+                    checked={this.props.task.checked}
+                    onClick={this.toggleChecked.bind(this)}
+                />
+
+                <span className="text">{this.props.task.text}</span>
+            </li>
         );
     }
 }
